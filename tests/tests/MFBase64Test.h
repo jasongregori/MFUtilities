9876cbfad076721ffa9a942fe966136c3f77f637
Jason Gregori <jasongregori@gmail.com>
2012-06-06 12:32:51 -0700

    Added test xcode project and tests for MFBase64

---
+++ tests/tests/MFBase64Test.h
@@ -0,0 +1,13 @@
+//
+//  MFBase64Test.h
+//  tests
+//
+//  Created by Jason Gregori on 6/4/12.
+//  Copyright (c) 2012 Jason Gregori. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MFBase64Test : SenTestCase
+
+@end
---
+++ tests/tests/MFBase64Test.m
@@ -0,0 +1,42 @@
+//
+//  MFBase64Test.m
+//  tests
+//
+//  Created by Jason Gregori on 6/4/12.
+//  Copyright (c) 2012 Jason Gregori. All rights reserved.
+//
+
+#import "MFBase64Test.h"
+
+#import "MFBase64.h"
+
+@implementation MFBase64Test
+
+- (void)testPadding {
+    STAssertEqualObjects([MFBase64 encodeString:@""], @"", nil);
+    STAssertEqualObjects([MFBase64 encodeString:@"a"], @"YQ==", nil);
+    STAssertEqualObjects([MFBase64 encodeString:@"bb"], @"YmI=", nil);
+    STAssertEqualObjects([MFBase64 encodeString:@"ccc"], @"Y2Nj", nil);
+    STAssertEqualObjects([MFBase64 encodeString:@"dddd"], @"ZGRkZA==", nil);
+    STAssertEqualObjects([MFBase64 encodeString:@"eeeee"], @"ZWVlZWU=", nil);
+    STAssertEqualObjects([MFBase64 encodeString:@"ffffff"], @"ZmZmZmZm", nil);
+}
+
+- (void)testNonASCII {
+    STAssertEqualObjects([MFBase64 encodeString:@"This is a heart: \u2665"], @"VGhpcyBpcyBhIGhlYXJ0OiDimaU=", nil);
+}
+
+- (void)testImage {
+    NSData *data = [NSData dataWithContentsOfFile:
+                    [[NSBundle bundleForClass:[self class]]
+                     pathForResource:@"Test Image" ofType:@"jpg"]];
+    NSString *encodedData = [NSString stringWithContentsOfFile:
+                             [[NSBundle bundleForClass:[self class]]
+                              pathForResource:@"Test Image Encoded" ofType:@"txt"]
+                                                  usedEncoding:NULL
+                                                         error:NULL];
+    STAssertEqualObjects([MFBase64 encodeData:data],
+                          encodedData, nil);
+}
+
+@end
